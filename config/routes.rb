c3bd0be387e759781cafb25564db09249d91cab7
Jimmy Fox <jimmy@jimmyfoxmusic.com>
2024-10-22 21:48:56 -0500

    get rid of provider_lookup namespace (undo last commit)

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  namespace "provider_lookup" do
-    # Defines the root path route ("/")
-    root "providers#index"
+  # Defines the root path route ("/")
+  root "providers#index"
   
-    resources :providers, only: [:index] do
-      collection do
-        put :find
-        get :physicians
-        get :clinics
-      end
+  resources :providers, only: [:index] do
+    collection do
+      put :find
+      get :physicians
+      get :clinics
     end
   end
 end
